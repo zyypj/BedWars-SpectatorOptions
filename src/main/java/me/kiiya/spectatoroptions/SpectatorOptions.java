1c3f31601da001f22a50b5530d1a9a1366ded5a5
Kiiya <105589176+KKiiya@users.noreply.github.com>
2023-12-28 18:44:20 +0100

    Initial Commit

---
+++ src/main/java/me/kiiya/spectatoroptions/SpectatorOptions.java
@@ -0,0 +1,17 @@
+package me.kiiya.spectatoroptions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpectatorOptions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
