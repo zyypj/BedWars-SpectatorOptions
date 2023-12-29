c5455b513245ceb7ffa495be38ee545e9aafc4fb
Kiiya <105589176+KKiiya@users.noreply.github.com>
2023-12-29 01:03:15 +0100

    Update ArenaJoin.java

--- src/main/java/me/kiiya/spectatoroptions/listeners/bedwars1058/ArenaJoin.java
+++ src/main/java/me/kiiya/spectatoroptions/listeners/bedwars1058/ArenaJoin.java
@@ -11,14 +11,38 @@ import org.bukkit.inventory.ItemStack;
 public class ArenaJoin implements Listener {
     @EventHandler
     public void onArenaJoin(PlayerJoinArenaEvent e) {
-        if (!e.isSpectator()) return;
         Player p = e.getPlayer();
+
+        if (!e.isSpectator()) return;
+
         ItemStack item = Utils.getOptionsItem(p);
         int slot = SpectatorOptions.getInstance().getMainConfig().getInt("options-item.slot");
 
         if (!p.hasPermission("bw.spectatoroptions")) return;
+
         Bukkit.getScheduler().runTaskLater(SpectatorOptions.getInstance(), () -> {
             p.getInventory().setItem(slot, item);
-       }, 20L);
+        }, 20L);
+
+        CachedOptions cachedOptions = SpectatorManager.getInstance().getCachedOptions(p);
+        if (cachedOptions == null) return;
+
+        if (cachedOptions.isToggleSpectators()) {
+            for (Player spectator : e.getArena().getSpectators()) {
+                p.hidePlayer(spectator);
+            }
+        } else {
+            for (Player spectator : e.getArena().getSpectators()) {
+                p.showPlayer(spectator);
+            }
+        }
+
+        if (cachedOptions.isNightVision()) {
+            p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0, false, false));
+        } else {
+            Bukkit.getScheduler().runTaskLater(SpectatorOptions, () -> {
+                p.removePotionEffect(PotionEffectType.NIGHT_VISION);
+            }, 20L);
+        }
     }
 }
