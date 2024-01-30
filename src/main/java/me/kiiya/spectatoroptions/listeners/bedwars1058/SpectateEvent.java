88a1aba6e5333d6da0211b64ce65c81d81befa6b
Kiiya <105589176+KKiiya@users.noreply.github.com>
2024-01-30 15:33:52 +0100

    Spectate event improvement

--- src/main/java/me/kiiya/spectatoroptions/listeners/bedwars1058/SpectateEvent.java
+++ src/main/java/me/kiiya/spectatoroptions/listeners/bedwars1058/SpectateEvent.java
@@ -1,6 +1,7 @@
 package me.kiiya.spectatoroptions.listeners.bedwars1058;
 
-import com.andrei1058.bedwars.api.BedWars;
+import com.andrei1058.bedwars.BedWars;
+import com.andrei1058.bedwars.api.events.spectator.SpectatorTeleportToPlayerEvent;
 import me.kiiya.spectatoroptions.SpectatorOptions;
 import me.kiiya.spectatoroptions.player.CachedOptions;
 import me.kiiya.spectatoroptions.player.SpectatorManager;
@@ -11,58 +12,32 @@ import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class SpectateEvent implements Listener {
     @EventHandler
-    public void onTeleport(PlayerTeleportEvent e) {
-        Player p = e.getPlayer();
-        if (e.getCause() != PlayerTeleportEvent.TeleportCause.PLUGIN) return;
-        if (e.getTo() == null) return;
-        if (e.getFrom().getWorld() != e.getTo().getWorld()) return;
-        if (e.getFrom().distance(e.getTo()) < 1) return;
-
-        BedWars.ArenaUtil arenaUtil = SpectatorOptions.getInstance().getBedWars1058API().getArenaUtil();
-        if (!arenaUtil.isSpectating(p)) return;
+    public void onTeleport(SpectatorTeleportToPlayerEvent e) {
+        Player p = e.getSpectator();
+        Player target = e.getTarget();
 
         CachedOptions cachedOptions = SpectatorManager.getInstance().getCachedOptions(p);
         if (cachedOptions == null) return;
 
-        try {
-            if (cachedOptions.isFirstPerson()) {
-                Player followingPlayer = (Player) e.getTo().getWorld().getNearbyEntities(e.getTo(), 1, 1, 1).stream().filter(entity -> entity instanceof Player).findFirst().orElse(null);
-                p.setGameMode(GameMode.SPECTATOR);
-                p.setSpectatorTarget(followingPlayer);
-                com.andrei1058.bedwars.BedWars.nms.sendTitle(p, Utils.getMsg(p, "spectator-first-person-enter-title").replace("%bw_player%", followingPlayer.getDisplayName()), Utils.getMsg(p, "spectator-first-person-enter-subtitle").replace("{player}", followingPlayer.getDisplayName()), 0, 20, 0);
-                cachedOptions.setFollowingPlayer(null);
-                if (cachedOptions.getFollowingTaskId() != -1) {
-                    Bukkit.getScheduler().cancelTask(cachedOptions.getFollowingTaskId());
-                    cachedOptions.setFollowingTaskId(-1);
-                }
-                return;
+        if (cachedOptions.isFirstPerson()) {
+            p.setGameMode(GameMode.SPECTATOR);
+            p.setSpectatorTarget(target);
+            BedWars.nms.sendTitle(p, Utils.getMsg(p, "spectator-first-person-enter-title").replace("%bw_player%", target.getDisplayName()), Utils.getMsg(p, "spectator-first-person-enter-subtitle").replace("{player}", target.getDisplayName()), 0, 20, 0);
+            cachedOptions.setFollowingPlayer(null);
+            if (cachedOptions.getFollowingTaskId() != -1) {
+                Bukkit.getScheduler().cancelTask(cachedOptions.getFollowingTaskId());
+                cachedOptions.setFollowingTaskId(-1);
             }
-        } catch (Exception ignored) {
-
+            return;
         }
 
         if (cachedOptions.isAutoTeleport()) {
-            if (e.getTo().getWorld().getNearbyEntities(e.getTo(), 1, 1, 1).stream().noneMatch(entity -> entity instanceof Player)) {
-                if (cachedOptions.getFollowingTaskId() != -1) {
-                    Bukkit.getScheduler().cancelTask(cachedOptions.getFollowingTaskId());
-                    cachedOptions.setFollowingTaskId(-1);
-                }
-                return;
-            }
+            cachedOptions.setFollowingPlayer(target);
 
-            Player followingPlayer = (Player) e.getTo().getWorld().getNearbyEntities(e.getTo(), 1, 1, 1).stream().filter(entity -> entity instanceof Player).findFirst().orElse(null);
-            cachedOptions.setFollowingPlayer(followingPlayer);
-
-            if (cachedOptions.getLastFollowingPlayer() == null) {
-                cachedOptions.setLastFollowingPlayer(followingPlayer);
-            }
-
-            if (followingPlayer == null) return;
-            if (followingPlayer != cachedOptions.getLastFollowingPlayer()) {
+            if (target != cachedOptions.getLastFollowingPlayer()) {
                 cachedOptions.setLastFollowingPlayer(null);
                 if (cachedOptions.getFollowingTaskId() != -1) {
                     Bukkit.getScheduler().cancelTask(cachedOptions.getFollowingTaskId());
